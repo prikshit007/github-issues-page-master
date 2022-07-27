284f616efd58bdeddd59a5fa21d124f55cdaae39
prikshit007 <prikshit762@gmail.com>
2022-07-27 08:29:44 +0530

    github issues project

---
+++ github-issues-page-master/src/api/constants.js
@@ -0,0 +1,13 @@
+
+
+export const WEB_URL = 'https://github.com';
+
+export const BASE_URL = 'https://api.github.com';
+
+export const OWNER =  'facebook';
+
+export const REPO = 'react';
+
+export const GIT_REPO_INFO = `${BASE_URL}/repos/${OWNER}/${REPO}`;
+
+export const GIT_ISSUE_ENDPOINT = `${GIT_REPO_INFO}/issues`;
